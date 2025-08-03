59f2dd4ff980ed8ddb8eaac53c0a18b7c4cbdfd9
Md. Toukir Ahmed Mehedi <cooltoukir@gmail.com>
2025-08-03 17:01:15 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/toukirahmed/riverpod_boilerplate/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.toukirahmed.riverpod_boilerplate
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/services/services.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:riverpod_boilerplate/models/user_model.dart';
+
+class ApiServices{
+  String endpoint = 'https://reqres.in/api/users?page=2';
+  Future<List<UserModel>> getUsers() async {
+    Response response = await get(Uri.parse(endpoint));
+    if (response.statusCode == 200) {
+      final List result = jsonDecode(response.body)['data'];
+      return result.map(((e) => UserModel.fromJson(e))).toList();
+    } else {
+      throw Exception(response.reasonPhrase);
+    }
+  }
+}
